69b72078d60e9bbb0d894ffd5ff47fac0741b103
정재민 <sai06266@naver.com>
2026-07-01 22:47:27 +0900

    refactor: mapper -> repository로 변경

--- src/main/java/com/ucamp/gyeongjuma_be/quiz/repository/QuizRepository.java
+++ src/main/java/com/ucamp/gyeongjuma_be/quiz/repository/QuizRepository.java
@@ -1,12 +1,14 @@
-package com.ucamp.gyeongjuma_be.quiz.mapper;
+package com.ucamp.gyeongjuma_be.quiz.repository;
 
 import com.ucamp.gyeongjuma_be.quiz.domain.Quiz;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Mapper
-public interface QuizMapper {
+@Repository
+public interface QuizRepository {
     // 전체 퀴즘 목록 조회
     List<Quiz> findAll();
 }
