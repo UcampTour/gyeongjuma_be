06542cf49593e61d005c514c4765c0e1d718bef8
정재민 <sai06266@naver.com>
2026-07-06 19:32:04 +0900

    feat: 퀴즈 상세 조회 + 다시 풀기 기능 구현

--- src/main/java/com/ucamp/gyeongjuma_be/quiz/repository/QuizRepository.java
+++ src/main/java/com/ucamp/gyeongjuma_be/quiz/repository/QuizRepository.java
@@ -1,8 +1,10 @@
 package com.ucamp.gyeongjuma_be.quiz.repository;
 
+import com.ucamp.gyeongjuma_be.quiz.dto.response.QuizDetailResponse;
 import com.ucamp.gyeongjuma_be.quiz.dto.response.lockedPlaceQuizDto;
 import com.ucamp.gyeongjuma_be.quiz.dto.response.visitedPlaceQuizDto;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,4 +17,8 @@ public interface QuizRepository {
     List<visitedPlaceQuizDto> findVisitedPlaceQuizList(Long memberId);
     // 미방문 장소 퀴즈 목록 조회
     List<lockedPlaceQuizDto> findLockedPlaceQuizList(Long memberId);
+    // 퀴즈 상세 조회
+    QuizDetailResponse findQuizDetailByPlaceId(@Param("placeId") Long placeId, @Param("memberId") Long memberId);
+    // 퀴즈 새로 풀기
+    void deleteQuizResponsesByPlaceId(@Param("placeId") Long placeId, @Param("memberId") Long memberId);
 }
