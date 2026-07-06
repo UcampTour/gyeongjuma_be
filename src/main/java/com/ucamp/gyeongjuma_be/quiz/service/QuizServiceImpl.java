3602482313abb18eae93691d6918aec2c7168925
정재민 <sai06266@naver.com>
2026-07-06 19:32:04 +0900

    feat: 퀴즈 상세 조회 + 다시 풀기 기능 구현

--- src/main/java/com/ucamp/gyeongjuma_be/quiz/service/QuizServiceImpl.java
+++ src/main/java/com/ucamp/gyeongjuma_be/quiz/service/QuizServiceImpl.java
@@ -1,11 +1,13 @@
 package com.ucamp.gyeongjuma_be.quiz.service;
 
+import com.ucamp.gyeongjuma_be.quiz.dto.response.QuizDetailResponse;
 import com.ucamp.gyeongjuma_be.quiz.dto.response.QuizListResponse;
 import com.ucamp.gyeongjuma_be.quiz.dto.response.lockedPlaceQuizDto;
 import com.ucamp.gyeongjuma_be.quiz.dto.response.visitedPlaceQuizDto;
 import com.ucamp.gyeongjuma_be.quiz.repository.QuizRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -25,5 +27,17 @@ public class QuizServiceImpl implements QuizService {
                 .visitedPlaceQuizDtoList(visitedPlaceQuizList)
                 .lockedPlaceQuizDtoList(lockedPlaceQuizList)
                 .build();
+    }
+
+    @Override
+    public QuizDetailResponse getQuizDetail(Long placeId, Long memberId) {
+        return quizRepository.findQuizDetailByPlaceId(placeId, memberId);
+    }
+
+    @Override
+    @Transactional
+    public QuizDetailResponse retryQuiz(Long placeId, Long memberId) {
+        quizRepository.deleteQuizResponsesByPlaceId(placeId, memberId);
+        return quizRepository.findQuizDetailByPlaceId(placeId, memberId);
     }
 }
