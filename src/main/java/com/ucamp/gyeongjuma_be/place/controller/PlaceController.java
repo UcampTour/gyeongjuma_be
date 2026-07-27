83abcd4c2e521bc12a0007b24c358e63662fcd1a
정재민 <sai06266@naver.com>
2026-07-27 21:03:58 +0900

    fix: /api 추가

--- src/main/java/com/ucamp/gyeongjuma_be/place/controller/PlaceController.java
+++ src/main/java/com/ucamp/gyeongjuma_be/place/controller/PlaceController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/place")
+@RequestMapping("/api/place")
 @RequiredArgsConstructor
 public class PlaceController {
 
